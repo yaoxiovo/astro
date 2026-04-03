f22f28a29ba60ca4ebc47dcec37930f80e468ec0
yaoxiovo <yaoxiovo@gmail.com>
2026-04-03 21:23:21 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -83,7 +83,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		// Leave this array empty to use the default favicon
 		{
-			src: "https://jpg.yaoxi.wiki/blog.yaoxi.xyz/home.png", // Path of the favicon, relative to the /public directory
+			src: "https://png.yaoxi.wiki/blog.yaoxi.xyz/home.png", // Path of the favicon, relative to the /public directory
 			//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
 			//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
 		},
@@ -126,7 +126,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "https:/jpg.yaoxi.wiki/blog.yaoxi.xyz/home.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "https:/png.yaoxi.wiki/blog.yaoxi.xyz/home.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "瑶曦",
 	bio: quotes[Math.floor(Math.random() * quotes.length)],
 	links: [
